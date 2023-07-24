166a1563f895f5efb5e097520fe82907a8d39939
bmn44100 <njerumurimi@gmail.com>
2023-07-24 12:42:52 -0700

    module for a Python script to export data for employee TODO list in the JSON format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,31 +4,54 @@ module for a Python script to export data
 for employee TODO list in the JSON format
 """
 
+
 import json
 import requests
 import sys
 
 
-def todolist_json():
+def employee_username(api_url, employee_id):
+    """
+    function that gets data from 
+    employee TODO list in the REST api
+    """
+    employee_name = requests.get(
+        "{}users/{}".format(api_url, employee_id), verify=False)
+    employee_list = employee_name.json()
+    return employee_list['username']
+
+
+def employee_todo_list(api_url):
     """
     function that exports data
     for employee TODO list in the JSON format
     """
-    employee_id = sys.argv[1]
-    employee = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                            .format(employee_id)).json()
-    list = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                        .format(employee_id)).json()
-    employee_name = employee.get("username")
-    with open("{}.json".format(employee_id), "w") as json_file:
-        task_data = {employee_id: []}
-        for todo_task in list:
-            todo_tasks = {"task": todo_task.get("title"),
-                          "completed": todo_task.get("completed"),
-                          "username": employee_name}
-            task_data[employee_id].append(todo_tasks)
-        json.dump(task_data, json_file)
+    employee_list = requests.get(
+        "{}todos".format(api_url), verify=False)
+    return employee_list.json()
 
 
-if __name__ == "__main__":
-    todolist_json()
+if __name__ == '__main__':
+    api_url = 'https://jsonplaceholder.typicode.com/'
+    dict_employees = {}
+    json_dict = {}
+    todo_list = employee_todo_list(api_url)
+
+    with open('todo_all_employees.json', 'w') as file:
+        user_name = None
+        for todo_task in todo_list:
+            employee_id = str(todo_task['userId'])
+            if employee_id in dict_employees.keys():
+                user_name = dict_employees[employee_id]
+            else:
+                user_name = employee_username(api_url, employee_id)
+                dict_employees[employee_id] = user_name
+                json_dict[employee_id] = []
+
+            todo = {}
+            todo['task'] = todo_task['title']
+            todo['completed'] = todo_task['completed']
+            todo['username'] = user_name
+            json_dict[employee_id].append(todo)
+
+        file.write(json.dumps(json_dict))
