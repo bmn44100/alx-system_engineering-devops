356ca6097f1cf722e435ab74621c954466618add
bmn44100 <njerumurimi@gmail.com>
2023-08-08 13:17:57 -0700

    pycodestyle

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import requests
 """
 queries the Reddit API and returns a list containing the
 titles of all hot articles for a given subreddit.
 """
+
+
+import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
