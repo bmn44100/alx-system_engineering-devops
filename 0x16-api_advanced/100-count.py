de4a93ac374cf31dd6c4e2ec845c41a15d5261c6
bmn44100 <njerumurimi@gmail.com>
2023-08-08 19:53:42 +0000

    recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import requests
+"""
+recursive function that queries the Reddit API, parses the
+title of all hot articles, and prints a
+sorted count of given keywords
+"""
+
+
+def count_words(subreddit, word_list):
+    """
+    parses the title of all hot articles, and prints a
+    sorted count of given keywords
+    """
+
+    reddit_api = "https://api.reddit.com/r/{}/hot".format(subreddit)
+    headers = {"User-Agent": "My User Agent Request"}
+
+    try:
+        api_url = requests.get(reddit_api, headers=headers).json()
+        after = api_url.get("data").get("after")
+        if after:
+            reddit_api = reddit_api + "?after={}".format(after)
+        articles = api_url.get("data").get("children")
+        for article in articles:
+            article.append(article.get("data").get("title"))
+        after = api_url.get("data").get("after")
+        if after is None:
+            return count
+        return count_words(subreddit, word_list)
+    except:
+        return None
